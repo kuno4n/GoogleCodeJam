8b6b838ecb5e4c900104d53cf61ff1620f4797cb
kuno4n <kuno4n@gmail.com>
2013-03-30 16:39:41 +0900

    20130330

---
+++ src/2012-Qual-B.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <numeric>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+#include <cmath>
+#include <cassert>
+#include <cstdarg>
+#include <sys/time.h>
+#include <fstream>
+//#include "cout.h"
+
+using namespace std;
+
+#define SZ(x) ((int)x.size())
+#define MSET(x,a) memset(x, a, (int)sizeof(x))
+#define PB push_back
+#define VI vector < int >
+#define PII pair < int, int >
+#define LL long long
+#define FOR(i,a,b) for (int i = (a); i < (b); i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(v) (v).begin(), (v).end()
+#define FIT(it,v) for (__typeof((v).begin()) it = (v).begin(); it != (v).end(); it++)
+#define OUT(A) cout << #A << " = "<< (A) << endl
+
+#define present(c, e) ((c).find((e)) != (c).end())
+#define cpresent(c, e) (find(all(c), (e)) != (c).end())
+
+int case_number;
+#define printg case_number++, printf("Case #%d: ",case_number), printf
+#define gout case_number++, printf("Case #%d: ",case_number), cout
+
+
+
+const int MAX_N = 100;
+int n, s, p, t[MAX_N];
+
+void init() {
+}
+
+void input() {
+	cin >> n >> s >> p;
+	REP(i, n) cin >> t[i];
+}
+
+void solve() {
+	int rem = s;
+	int res = 0;
+	int mn1 = max(0, p-1);
+	int mn2 = max(0, p-2);
+	REP(i, n){
+		if(t[i] >= p + mn1*2) res++;
+		else if(t[i] >= p + mn2*2 && rem > 0){
+			res++;
+			rem--;
+		}
+	}
+	gout << res << endl;
+}
+
+
+
+int main() {
+	int number_of_test_cases;
+	scanf("%d",&number_of_test_cases);
+	REP(i,number_of_test_cases){
+		input();
+		solve();
+	}
+	return 0;
+}

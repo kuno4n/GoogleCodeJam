2ec815c73721a3dbe951fe738b85c41298e8a559
kuno.shinya <kuno4n@gmail.com>
2019-04-08 10:14:20 +0900

    20190407

---
+++ 2019-qual-A.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <numeric>
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cctype>
+#include <cmath>
+#include <cassert>
+#include <cstdarg>
+#include <sys/time.h>
+#include <fstream>
+//#include "cout.h"
+
+using namespace std;
+
+#define SZ(x) ((int)x.size())
+#define MSET(x,a) memset(x, a, (int)sizeof(x))
+#define PB push_back
+#define VI vector < int >
+#define PII pair < int, int >
+#define LL long long
+#define FOR(i,a,b) for (int i = (a); i < (b); i++)
+#define REP(i,n) FOR(i,0,n)
+#define ALL(v) (v).begin(), (v).end()
+#define FIT(it,v) for (__typeof((v).begin()) it = (v).begin(); it != (v).end(); it++)
+#define OUT(A) cout << #A << " = "<< (A) << endl
+#define OUT2(A, B) cout << "(" << #A << ", " << #B << ") = (" << (A) << ", "<< (B) << ")" << endl
+template<class T> void chmin(T &t, T f) { if (t > f) t = f; }
+template<class T> void chmax(T &t, T f) { if (t < f) t = f; }
+
+#define present(c, e) ((c).find((e)) != (c).end())
+#define cpresent(c, e) (find(all(c), (e)) != (c).end())
+
+int case_number;
+#define printg case_number++, printf("Case #%d: ",case_number), printf
+#define gout case_number++, printf("Case #%d: ",case_number), cout
+
+string s;
+
+void init() {
+}
+
+void input() {
+  cin >> s;
+}
+
+void solve() {
+  string s1, s2;
+  REP(i, SZ(s)) {
+    if(s[i] == '4') {
+      s1 += '2';
+      s2 += '2';
+    }
+    else if  (s[i] == '0') {
+      if (s1 != "") s1 += '0';
+      if (s2 != "") s2 += '0';
+    }
+    else {
+      if (s1 == "") {
+        s1 += s[i];
+        if (s2 != "") {
+          s2 += '0';
+        }
+      }
+      else {
+        s2 += s[i];
+        if (s1 != "") {
+          s1 += '0';
+        }
+      }
+    }
+  }
+  gout << s1 << " " << s2 << endl;
+}
+
+int main() {
+	init();
+	int number_of_test_cases;
+	scanf("%d",&number_of_test_cases);
+	REP(i,number_of_test_cases){
+		input();
+		solve();
+	}
+	return 0;
+}
